b224c5532c3efb379e0d50bc64ec09c0d9e9d2fb
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-18 00:49:16 +0200

    konkatenacija

---
+++ ex04/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // change code below this line
+    var myString = "This is the first string. " + "This is the second string.";
+    // change code above this line
+
+    return myString;
+
+}
+
+console.log(main());
+module.exports = main;
