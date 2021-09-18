99c81472baa1aa4f01941d181e0b55639c9882ac
jasminHadzialic <hadzialic.jasmin@gmail.com>
2021-09-18 10:20:17 +0200

    word blank

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change code below this line
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks = "Yesterday my " + myNoun  + " Boby " + myVerb + " away with a " + myAdjective + " bone in his mouth, which he dug up " + myAdverb + " from the yard.";
+    // Only change code above this line
+    return wordBlanks;
+
+}
+
+console.log(main());
+module.exports = main;
